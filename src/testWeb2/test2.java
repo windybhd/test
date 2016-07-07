d9ca8ecd1ddab34187b652f2f15a9ab43c2a3a6a
chen zhang <windyzj@163.com>
2016-07-08 01:05:44 +0800

    A2

--- src/testWeb2/test2.java
+++ src/testWeb2/test2.java
@@ -5,7 +5,7 @@ public class test2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("121212");
-		System.out.println("featureA ");
+		System.out.println("featureA2 ");
 	}
 
 	
