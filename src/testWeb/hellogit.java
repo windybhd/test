6d6ca06d9315a4c426f522bcff96cd9f072430a4
chen zhang <windyzj@163.com>
2016-07-08 00:59:15 +0800

    Revert "fix ln"
    This reverts commit dd5d46459e0cfb7c17bebb7b798e7f88319beba2.

--- src/testWeb/hellogit.java
+++ src/testWeb/hellogit.java
@@ -4,14 +4,10 @@ public class hellogit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
- 
-		System.out.print("hello git!!@#@??fffffffffadfaf?");
-		System.out.print("hello git!!@#@??ff232323f?");
+		System.out.println("hello git!!@#@??fffffffffadfaf?");
+		System.out.println("hello git!!@#@??ff232323f?");
 		System.out.println("feature A !");
 		System.out.println("feature A2 !");
- 
-
- 
 	}
 
 }
