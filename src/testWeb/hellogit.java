823fe134a3650720a12e7de0753fc07e21eacc83
chen zhang <windyzj@163.com>
2016-07-07 23:54:30 +0800

    feature2!

--- src/testWeb/hellogit.java
+++ src/testWeb/hellogit.java
@@ -6,6 +6,7 @@ public class hellogit {
 		// TODO Auto-generated method stub
 		System.out.println("hello git!!@#@??fffffffffadfaf?");
 		System.out.println("hello git!!@#@??ff232323f?");
+		System.out.println("feature A !");
 	}
 
 }
