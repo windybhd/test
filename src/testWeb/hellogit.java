dd5d46459e0cfb7c17bebb7b798e7f88319beba2
chen zhang <windyzj@163.com>
2016-07-08 00:04:37 +0800

    fix ln
    Conflicts:
    	src/testWeb/hellogit.java

--- src/testWeb/hellogit.java
+++ src/testWeb/hellogit.java
@@ -4,10 +4,14 @@ public class hellogit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello git!!@#@??fffffffffadfaf?");
-		System.out.println("hello git!!@#@??ff232323f?");
+ 
+		System.out.print("hello git!!@#@??fffffffffadfaf?");
+		System.out.print("hello git!!@#@??ff232323f?");
 		System.out.println("feature A !");
 		System.out.println("feature A2 !");
+ 
+
+ 
 	}
 
 }
